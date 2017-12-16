010d9df596d4e6a53883847afab1243ee9c603ec
Adridg <adrdomgon@gmail.com>
2017-12-16 15:52:34 +0100

    Añadido el fichero HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class HolaMundo 
+{
+	   public static void main (String args[])
+	   {
+	     JOptionPane.showMessageDialog(null,"Hola Mundo");   
+	   }
+}
