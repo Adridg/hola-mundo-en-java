52bd47f58f23f0e6c7a2246bcbaf5593d754603a
Adridg <adrdomgon@gmail.com>
2017-12-16 16:02:12 +0100

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,14 @@
 import javax.swing.JOptionPane;
+/**
+ * Muestra por pantalla la frase "¡Hola Mundo!" 
+ *
+ * @author Adrian Dominguez Gonzalez
+ */
 
 public class HolaMundo 
 {
 	   public static void main (String args[])
 	   {
-	     JOptionPane.showMessageDialog(null,"Hola Mundo");   
+	     JOptionPane.showMessageDialog(null,"¡Hola Mundo!");   
 	   }
 }
